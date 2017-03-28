d49dce79ae1be91db7b0bb4042582e3da1dec989
EvgeniyBespalov <bespalov1299@mail.ru>
2017-03-28 20:37:43 +0300

    pre relise

---
+++ line.rb
@@ -0,0 +1,12 @@
+class Line
+  
+  attr_accessor :pointA, :pointB, :angile, :b
+  
+  def initialize(pointA, pointB, angile, b)
+    @pointA = pointA
+    @pointB = pointB
+    @angile = angile
+    @b = b
+  end
+
+end
---
+++ lines.rb
@@ -0,0 +1,9 @@
+class Lines
+  
+  attr_accessor :lines
+  
+  def initialize(lines)
+    @lines = lines
+  end
+
+end
