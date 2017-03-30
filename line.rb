08b7c30e00837c4357ee950289b485abd4c2b325
EvgeniyBespalov <bespalov1299@mail.ru>
2017-03-30 12:28:49 +0300

    incapsulate logic in line

--- line.rb
+++ line.rb
@@ -2,11 +2,19 @@ class Line
   
   attr_accessor :point_a, :point_b, :angle, :b, :verticale
   
-  def initialize(point_a, point_b, angle, b, verticale)
+  def initialize(point_a, point_b)
     @point_a = point_a
     @point_b = point_b
-    @angle = angle
-    @b = b
+    
+    verticale = @point_a.x == @point_b.x
+  
+    if verticale
+      @b = @point_a.x
+    else
+      @angle = (@point_a.y.to_f - @point_b.y.to_f)/(@point_a.x.to_f - @point_b.x.to_f)
+      @b = @point_a.y.to_f - @point_a.x.to_f * @angle.to_f
+    end    
+    
   end
 
 end
