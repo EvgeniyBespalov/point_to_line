9888202abf0cc45de2f91da974178d5c6c3f46cc
EvgeniyBespalov <bespalov1299@mail.ru>
2017-04-04 13:31:07 +0300

    remove initialize

--- search_line.rb
+++ search_line.rb
@@ -3,74 +3,72 @@ require_relative 'line.rb'
 
 class SearchLine
 
-  #инициализация переменных
-  def initialize
-    
-    #массив входных точек
-    @array_point = []
-    
-    #массив всех допустимых линий
-    @line_list = []
-    
-    #массив результирующих точек
-    @points_result = []
-    
-    #массив результирующих линий
-    @lines_result = []         
-      
-  end
-  
   #search line from three or more point
-  def search(point_array)
+  def search(points)
   
-    get_point point_array
+    point_array = get_point(points)
     
-    create_all_line
+    line_array = create_all_line point_array
     
-    get_valid_point
-    
-    return @lines_result
+    return get_valid_point line_array
     
   end
   
   #заполняем массив точек данными из входного массива
-  def get_point point_array
-      
-    point_array.each do |element|
+  def get_point(points)
+    
+    point_array = []
+    
+    points.each do |element|
       temp_array = element.split ','
-      @array_point.push(Point.new temp_array[0], temp_array[1].to_i, temp_array[2].to_i)
+      point_array.push(Point.new temp_array[0], temp_array[1].to_i, temp_array[2].to_i)
     end   
+    
+    return point_array
     
   end
   
   #создаем все допустимые линии
-  def create_all_line
-    @array_point.each do |point_a|
-      @array_point.select{|x| x != point_a}.each do |point_b|
-        if ((@line_list.select{|l| l.point_a == point_a && l.point_b == point_b}).length == 0 &&
-          (@line_list.select{|l| l.point_a == point_b && l.point_b == point_a}).length == 0)
-            @line_list.push(Line.new(point_a, point_b))
+  def create_all_line(point_array)
+  
+    line_array = []
+     
+    point_array.each do |point_a|
+      point_array.select{|x| x != point_a}.each do |point_b|
+        if ((line_array.select{|l| l.point_a == point_a && l.point_b == point_b}).length == 0 &&
+          (line_array.select{|l| l.point_a == point_b && l.point_b == point_a}).length == 0)
+            line_array.push(Line.new(point_a, point_b))
         end
       end
     end
+    
+    return line_array
+    
   end
   
     #выбираем точки из линий в которых смещение и угол совпадают, с количеством линий больше 1
-  def get_valid_point
-    @line_list.uniq{ |x| [x.angle, x.b, x.verticale] }.each do |angle|
-      if @line_list.select {|l| (l.angle == angle.angle && l.b == angle.b && l.verticale == angle.verticale) }.count > 1
-        @points_result.clear       
+  def get_valid_point(line_array)
+
+    points_result = []    
+    lines_result = []
+    
+    line_array.uniq{ |x| [x.angle, x.b, x.verticale] }.each do |angle|
+      if line_array.select {|l| (l.angle == angle.angle && l.b == angle.b && l.verticale == angle.verticale) }.count > 1
+        points_result.clear       
                 
-        for line in @line_list.select {|l| l.angle == angle.angle && l.b == angle.b && l.verticale == angle.verticale}
-            @points_result.push(line.point_a)
-            @points_result.push(line.point_b)
+        for line in line_array.select {|l| l.angle == angle.angle && l.b == angle.b && l.verticale == angle.verticale}
+            points_result.push(line.point_a)
+            points_result.push(line.point_b)
         end
         
-        @points_result = @points_result.uniq
-        @lines_result.push(@points_result.sort_by{ |p| p.name })
+        points_result = points_result.uniq
+        lines_result.push(points_result.sort_by{ |p| p.name })
 
       end   
-    end  
+    end
+    
+    return lines_result
+      
   end
   
 end
